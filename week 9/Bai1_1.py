47b84f6172efb670dae751d399538095430b2995
nhingocanh <25112095@st.vju.ac.vn>
2026-04-23 15:03:33 +0700

    Add files via upload

---
+++ week 9/Bai1_1.py
@@ -0,0 +1,33 @@
+import math
+
+class Point:
+    def __init__(self, x=0, y=1):
+        self.__x, self.__y = x, y
+
+    def read(self):
+        self.__x, self.__y = map(int, input().split())
+
+    def print(self):
+        print(f"({self.__x}, {self.__y})")
+
+    def move(self, dx, dy):
+        self.__x += dx
+        self.__y += dy
+
+    def getX(self):
+        return self.__x
+
+    def getY(self):
+        return self.__y
+
+    def distance(self, p=None):
+        if p is None:
+            return math.sqrt(self.__x**2 + self.__y**2)
+        else:
+            return math.sqrt((p.getX() - self.__x)**2 + (p.getY() - self.__y)**2)
+    
+# chạy thử
+p1= Point (3, 4)
+p2 = Point (6, 8)
+print(p1.distance())
+print(p1.distance(p2))
